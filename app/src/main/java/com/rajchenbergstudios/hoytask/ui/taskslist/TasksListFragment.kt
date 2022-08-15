a571dcc572d73ce43484e2991ed69973048470cc
David-Rajchenberg <david.rajchenberg@fiverr.com>
2022-08-15 16:20:44 +0300

    003 Navigation component *** 1) Added navigation resource package 2) Added nav_graph file to navigation resource package 3) Created TasksListFragment and ViewModel 4) Hooked TasksListFragment to its xml view 5) Added TasksListFragment to the nav_graph 4) Added the Navigation fragment container to the MainActivity.xml file ***

---
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskslist/TasksListFragment.kt
@@ -0,0 +1,8 @@
+package com.rajchenbergstudios.hoytask.ui.taskslist
+
+import androidx.fragment.app.Fragment
+import com.rajchenbergstudios.hoytask.R
+
+class TasksListFragment : Fragment(R.layout.fragment_tasks_list){
+
+}
---
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskslist/TasksListViewModel.kt
@@ -0,0 +1,4 @@
+package com.rajchenbergstudios.hoytask.ui.taskslist
+
+class TasksListViewModel {
+}
