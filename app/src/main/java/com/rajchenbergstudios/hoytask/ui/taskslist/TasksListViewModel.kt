b4824bd0521421c32a51984c3340d23b5750cbfa
David-Rajchenberg <david.rajchenberg@fiverr.com>
2022-08-18 18:38:27 +0300

    006 Room search *** 1) Built xml menu file 2) Connected xml menu to fragment with MenuHost and MenuProvider 3) Build a OnQueryTextChanged extension function for catching data on key stroke 4) Reconfigured getTasks() query to be more specific 5) Made a flow for collecting list of tasks on every key stroke in view model and made it accesible as LiveData for fragment 6) Connected tasksFlow to searchView on fragment through view model ***

--- app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskslist/TasksListViewModel.kt
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskslist/TasksListViewModel.kt
@@ -4,12 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.rajchenbergstudios.hoytask.data.task.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @HiltViewModel
 class TasksListViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel(){
 
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksFlow = searchQuery.flatMapLatest { tasksList ->
+        taskDao.getTasks(tasksList)
+    }
+
+    val tasks = tasksFlow.asLiveData()
 }
