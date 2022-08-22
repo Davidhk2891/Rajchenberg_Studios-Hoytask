b10775d309745e7931d470812824154b91948627
David-Rajchenberg <david.rajchenberg@fiverr.com>
2022-08-22 14:03:25 +0300

    012 Add Edit logic *** 1) Build all feautres for listening to text input and checkbox input in TaskAddEditFragment 2) Made sure import is recorded in taskName and taskImportance saveStateHandle vars 3) Added events to event collector for catching event for back navigation with code andfor invalid input in taskName 4) Added event wrapper class for invalid input and for navigate back with code after task updated or created 5) Added event in tasksListViewModel to dispatch input task created or task updated message 6) Sent data back with setFragmentResult and setFragmentResultListener carrying the result from TaskAddEditFragment after task was added/updated ***

--- app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskaddedit/TaskAddEditViewModel.kt
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskaddedit/TaskAddEditViewModel.kt
@@ -1,11 +1,20 @@
 package com.rajchenbergstudios.hoytask.ui.taskaddedit
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rajchenbergstudios.hoytask.data.task.Task
 import com.rajchenbergstudios.hoytask.data.task.TaskDao
+import com.rajchenbergstudios.hoytask.ui.ADD_TASK_RESULT_OK
+import com.rajchenbergstudios.hoytask.ui.EDIT_TASK_RESULT_OK
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+private const val TAG = "TaskEditViewModel.kt"
 
 @HiltViewModel
 class TaskAddEditViewModel @Inject constructor(
@@ -15,15 +24,57 @@ class TaskAddEditViewModel @Inject constructor(
 
     val task = state.get<Task>("task")
 
-    var taskName = state.get<String>("taskName") ?: task?.name ?: " "
+    var taskName = state.get<String>("taskName") ?: task?.name ?: ""
         set(value) {
             field = value
-            state.set("taskName", value)
+            state["taskName"] = value
         }
 
     var taskImportance = state.get<Boolean>("taskImportance") ?: task?.important ?: false
         set(value) {
             field = value
-            state.set("taskName", value)
+            state["taskName"] = value
         }
+
+    // AddEdit event channel
+    private val addEditEventChannel = Channel<AddEditEvent>()
+
+    // AddEdit event
+    val addEditEvent = addEditEventChannel.receiveAsFlow()
+
+    fun onSaveClick(){
+
+        if (taskName.isBlank()) {
+            Log.i(TAG, "taskName: $taskName")
+            showInvalidInputMessage()
+            return
+        }
+
+        if (task != null) {
+            val updatedTask = task.copy(name = taskName, important = taskImportance)
+            updateTask(updatedTask)
+        } else {
+            val newTask = Task(name = taskName, important = taskImportance)
+            createTask(newTask)
+        }
+    }
+
+    private fun createTask(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+        addEditEventChannel.send(AddEditEvent.NavigateBackWithResult(ADD_TASK_RESULT_OK))
+    }
+
+    private fun updateTask(task: Task) = viewModelScope.launch {
+        taskDao.update(task)
+        addEditEventChannel.send(AddEditEvent.NavigateBackWithResult(EDIT_TASK_RESULT_OK))
+    }
+
+    private fun showInvalidInputMessage() = viewModelScope.launch {
+        addEditEventChannel.send(AddEditEvent.ShowInvalidInputMessage("Name cannot be empty"))
+    }
+
+    sealed class AddEditEvent {
+        data class ShowInvalidInputMessage(val msg: String) : AddEditEvent()
+        data class NavigateBackWithResult(val result: Int) : AddEditEvent()
+    }
 }
