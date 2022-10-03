1079543aad061b8aee7aeb5e3b9f90a3418c181a
David-Rajchenberg <david.rajchenberg@fiverr.com>
2022-10-03 18:02:48 +0300

    Added swipe to delete and undo functionality for tasks inside a set

--- app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskssetedit/TasksInSetListFragment.kt
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskssetedit/TasksInSetListFragment.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.rajchenbergstudios.hoytask.R
-import com.rajchenbergstudios.hoytask.databinding.FragmentTasksSetEditBinding
+import com.rajchenbergstudios.hoytask.databinding.FragmentTasksInSetBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TasksInSetListFragment : Fragment(R.layout.fragment_tasks_set_edit) {
+class TasksInSetListFragment : Fragment(R.layout.fragment_tasks_in_set) {
 
     private val viewModel: TasksInSetListViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentTasksSetEditBinding.bind(view)
+        val binding = FragmentTasksInSetBinding.bind(view)
 
         val tasksInSetListAdapter = TasksInSetListAdapter()
 
@@ -27,10 +31,41 @@ class TasksInSetListFragment : Fragment(R.layout.fragment_tasks_set_edit) {
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
             }
+
+            ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val taskInSet = tasksInSetListAdapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onTaskInSetSwiped(taskInSet)
+                }
+            }).attachToRecyclerView(tasksSetDetailsRecyclerview.layoutTasksListRecyclerview)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.tasksInSetsEvent.collect { event ->
+                when (event) {
+                    is TasksInSetListViewModel.TaskInSetEvent.ShowUndoDeleteTaskMessage -> {
+                        Snackbar
+                            .make(requireView(), "Task deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO"){
+                                viewModel.onUndoDeleteClick(event.taskInSet)
+                            }
+                            .show()
+                    }
+                }
+            }
         }
 
         viewModel.tasksSet?.observe(viewLifecycleOwner) { tasksInSetList ->
-            // The list to submit is to be that of only the actual tasks in the set, not the whole TaskSet item
             tasksInSetListAdapter.submitList(tasksInSetList)
         }
     }
--- app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskssetedit/TasksInSetListViewModel.kt
+++ app/src/main/java/com/rajchenbergstudios/hoytask/ui/taskssetedit/TasksInSetListViewModel.kt
@@ -3,9 +3,14 @@ package com.rajchenbergstudios.hoytask.ui.taskssetedit
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rajchenbergstudios.hoytask.data.taskinset.TaskInSet
 import com.rajchenbergstudios.hoytask.data.taskinset.TaskInSetDao
 import com.rajchenbergstudios.hoytask.data.taskset.TaskSet
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,11 +19,30 @@ class TasksInSetListViewModel @Inject constructor(
     state: SavedStateHandle
 ) : ViewModel(){
 
+    // Tasks in set channel
+    private val tasksInSetsChannel = Channel<TaskInSetEvent>()
+
+    // Tasks in set event
+    val tasksInSetsEvent = tasksInSetsChannel.receiveAsFlow()
+
     private val taskSet = state.get<TaskSet>("taskset")
 
     private val taskSetTitle = state.get<String>("taskSetTitle") ?: taskSet?.title
 
     val tasksSet = taskSetTitle?.let {
         taskInSetDao.getTasksInSet(taskSetTitle).asLiveData()
+    }
+
+    fun onTaskInSetSwiped(taskInSet: TaskInSet) = viewModelScope.launch {
+        taskInSetDao.delete(taskInSet)
+        tasksInSetsChannel.send(TaskInSetEvent.ShowUndoDeleteTaskMessage(taskInSet))
+    }
+
+    fun onUndoDeleteClick(taskInSet: TaskInSet) = viewModelScope.launch {
+        taskInSetDao.insert(taskInSet)
+    }
+
+    sealed class TaskInSetEvent {
+        data class ShowUndoDeleteTaskMessage(val taskInSet: TaskInSet) : TaskInSetEvent()
     }
 }
