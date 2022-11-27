9123cd5ca9316f682423ad314e2369027d13719c
David-Rajchenberg <david.rajchenberg@fiverr.com>
2022-11-27 11:10:21 +0200

    Journal entries now display on respective screen

--- app/src/main/java/com/rajchenbergstudios/hoygenda/data/today/journalentry/JournalEntry.kt
+++ app/src/main/java/com/rajchenbergstudios/hoygenda/data/today/journalentry/JournalEntry.kt
@@ -3,11 +3,13 @@ package com.rajchenbergstudios.hoygenda.data.today.journalentry
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import kotlinx.parcelize.Parcelize
 import java.text.DateFormat
 
 @Entity(tableName = "journal_table")
 @Parcelize
+@JsonIgnoreProperties(ignoreUnknown = true)
 class JournalEntry(
     val content: String,
     val important: Boolean = false,
@@ -15,5 +17,5 @@ class JournalEntry(
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 ) : Parcelable {
     val createdTimeFormat: String
-        get() = DateFormat.getTimeInstance().format(created)
+        get() = DateFormat.getTimeInstance(DateFormat.SHORT).format(created)
 }
